502b0c8da916aeb8b6d4f162d21f817d2eb228f7
Veda <619veda@gmail.com>
2025-10-28 12:06:37 -0400

    sprint3

--- src/main/java/edu/iu/p566/MyTV/service/VideoService.java
+++ src/main/java/edu/iu/p566/MyTV/service/VideoService.java
@@ -20,6 +20,7 @@ public class VideoService {
     }
 
     public void saveVideos(Schedule schedule) {
+        schedule.setYoutubeLink(convertVideoLink(schedule.getYoutubeLink()));
         repo.save(schedule);
     }
 
@@ -28,6 +29,20 @@ public class VideoService {
     }
 
     public void updateVideo(Schedule schedule) {
+        schedule.setYoutubeLink(convertVideoLink(schedule.getYoutubeLink()));
         repo.update(schedule);
+    }
+
+    public String convertVideoLink(String url) {
+        if (url == null)
+            return url;
+        if (url.contains("embed"))
+            return url;
+        if (url.contains("watch?v=")) {
+            return url.replace("watch?v=", "embed/");
+        } else if (url.contains("youtu.be/")) {
+            return url.replace("youtu.be/", "youtube.com/embed/");
+        }
+        return url;
     }
 }
