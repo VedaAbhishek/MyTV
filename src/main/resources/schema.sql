89e3ed9bca6450a83f2361d292e693ebf9ef08c0
Veda <619veda@gmail.com>
2025-11-04 14:49:29 -0500

    sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS videos  (
-	"id"	INTEGER,
-	"youtube_link"	TEXT NOT NULL,
-	"duration_seconds"	INTEGER,
-	"scheduled_date"	TEXT,
-	"scheduled_time"	TEXT,
-	PRIMARY KEY("id")
+CREATE TABLE IF NOT EXISTS videos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    youtube_link TEXT NOT NULL,
+    duration_seconds INTEGER NOT NULL,
+    scheduled_date TEXT NOT NULL,
+    scheduled_time TEXT NOT NULL
 );
