a1aa59d97a25e930e601e06b94c38b30372a3f4f
Veda <619veda@gmail.com>
2025-11-04 21:50:53 -0500

    Changes to properties file

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS videos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    youtube_link TEXT NOT NULL,
-    duration_seconds INTEGER NOT NULL,
-    scheduled_date TEXT NOT NULL,
-    scheduled_time TEXT NOT NULL
-);
